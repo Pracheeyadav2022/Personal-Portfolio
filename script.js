e1474b0e659a539ffb5fbc2bbadf3e9e898f07d5
Pracheeyadav2022 <108492578+Pracheeyadav2022@users.noreply.github.com>
2023-07-23 14:57:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 <script src="https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js"></script>
+<script>
+        var typed = new Typed('#element', {
+            strings: ['Software developer', 'Web Developer', 'Web Designer'],
+            typeSpeed: 50,
+
+        });
+    </script>
